6c2f399d55e6fa33ddca9c3853efa371b9c12e0a
abhi <abhijeetvc7@gmail.com>
2019-07-21 13:49:23 +0530

    Entity relations changes

--- src/main/java/com/julyjpabatch/julyjpa/model/Category.java
+++ src/main/java/com/julyjpabatch/julyjpa/model/Category.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 //@Data
@@ -17,6 +19,8 @@ public class Category {
 
     @Id
     private Integer id;
+    @NotNull
+    @NotBlank
     private String categoryName;
 
     @OneToMany(mappedBy = "category")
